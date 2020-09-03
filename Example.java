81635cacd7d4a5b5bdd2e7187ffd1edecf47ded4
pogrebnoyanton85 <69414855+pogrebnoyanton85@users.noreply.github.com>
2020-09-03 21:46:04 +0300

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,18 @@
+// Вывести все нечетные числа от 0 до 200
+public class Example
+{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<=200;i++)
+        {
+            if(i%3==0)
+            {
+                System.out.println(i);
+            }
+
+        }
+        
+
+    }
+
+}
